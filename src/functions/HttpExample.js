7b2bc121972a78c89c1bb7eadabf401e83418ebc
KeerthanaGarimella <kgarimella5513@conestogac.on.ca>
2025-08-05 18:09:54 -0400

    updated  HttpExample.js

--- src/functions/HttpExample.js
+++ src/functions/HttpExample.js
@@ -1,6 +1,13 @@
-module.exports = async function (context, req) {
-    context.res = {
-        status: 200,
-        body: "Hello, world!",
-    };
-};
+const { app } = require('@azure/functions');
+
+app.http('HttpExample', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
